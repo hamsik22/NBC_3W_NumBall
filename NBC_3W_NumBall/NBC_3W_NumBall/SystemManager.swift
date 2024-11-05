429d52ecf80a129294ad31bd8d1f3879e087448a
HwangSukHyeon <payback9488@gmail.com>
2024-11-05 10:23:44 +0900

    [File]: Add SystemManager.swift

---
+++ NBC_3W_NumBall/NBC_3W_NumBall/SystemManager.swift
@@ -0,0 +1,13 @@
+//
+//  SystemManager.swift
+//  NBC_3W_NumBall
+//
+//  Created by 황석현 on 11/5/24.
+//
+
+import Foundation
+
+/// 시스템 매니져의 이름을 가진만큼 프로그램 동작에 대한 관리를 한다.
+class SystemManager {
+    
+}
